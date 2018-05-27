a6e5a9f709b3087e1ea811d2a1e9e62d0e62db09
Alberto <albrivas95@outlook.es>
2018-05-27 20:43:15 +0200

    Cargar video desde Youtube - Completado

---
+++ Gym_App/app/src/main/java/retamar/com/gym_app/API/YoutubeConfig.java
@@ -0,0 +1,6 @@
+package retamar.com.gym_app.API;
+
+public class YoutubeConfig {
+
+    public final static String KEY_DEVELOPER = "AIzaSyA72jWy8EcFEYPQr0eFyhJOEh1YkCfkZgk";
+}
--- Gym_App/app/src/main/java/retamar/com/gym_app/DescripcionEjercicio.java
+++ Gym_App/app/src/main/java/retamar/com/gym_app/DescripcionEjercicio.java
@@ -1,5 +1,6 @@
 package retamar.com.gym_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,20 +11,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+import retamar.com.gym_app.API.YoutubeConfig;
 import retamar.com.gym_app.utils.Ejercicios;
 
-public class DescripcionEjercicio extends YouTubeBaseActivity {
+public class DescripcionEjercicio extends AppCompatActivity {
 
-    Ejercicios ejercicio;
-    YouTubePlayerView youTubePlayerView;
-    YouTubePlayer.OnInitializedListener listener;
-    TextView descripcion;
+    private Ejercicios ejercicio;
+    private TextView descripcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,31 +34,57 @@ public class DescripcionEjercicio extends YouTubeBaseActivity {
         instancias();
         configurarToolbar();
         rellenar();
+        videoYoutube();
+    }
+
+    private void videoYoutube() {
+        YouTubePlayerFragment youtubeFragment = (YouTubePlayerFragment)
+                getFragmentManager().findFragmentById(R.id.youtubeFragment);
+
+        youtubeFragment.initialize(YoutubeConfig.KEY_DEVELOPER,
+                new YouTubePlayer.OnInitializedListener() {
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                        YouTubePlayer youTubePlayer, boolean b) {
+                        youTubePlayer.cueVideo(ejercicio.getVideo());
+                    }
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult youTubeInitializationResult) {
+
+                    }
+                });
     }
 
     private void rellenar() {
         if(ejercicio != null) {
-            descripcion.setText(ejercicio.getDescripcion());
-            Toast.makeText(this, ejercicio.getDescripcion(), Toast.LENGTH_LONG).show();
+            descripcion.setText(ejercicio.getNombre());
         }
     }
 
     private void instancias() {
         ejercicio = (Ejercicios) getIntent().getExtras().getSerializable(TipoEjercicio.TAG_EJERCICIO);
         descripcion = findViewById(R.id.descripcion);
+
     }
 
     private void configurarToolbar() {
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         if(getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(Principal.TAG_EJERCICIO);
+            getSupportActionBar().setTitle(TipoEjercicio.TAG_EJERCICIO);
         }
 
         // Boton volver atras.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);*/
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
