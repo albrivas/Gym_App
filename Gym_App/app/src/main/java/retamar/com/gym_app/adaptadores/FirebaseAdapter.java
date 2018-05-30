3b0933ff93d42532db7b2061ad883d8044a02f2e
Alberto <albrivas95@outlook.es>
2018-05-30 23:45:53 +0200

    Numero de ejercicios por tipo de ejercicio -> Completado

--- Gym_App/app/src/main/java/retamar/com/gym_app/adaptadores/FirebaseAdapter.java
+++ Gym_App/app/src/main/java/retamar/com/gym_app/adaptadores/FirebaseAdapter.java
@@ -8,8 +8,12 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import retamar.com.gym_app.R;
 import retamar.com.gym_app.utils.Ejercicios;
@@ -27,7 +31,7 @@ public class FirebaseAdapter extends FirebaseRecyclerAdapter<Ejercicios, Ejercic
 
 
     @Override
-    protected void populateViewHolder(EjerciciosHolder viewHolder, final Ejercicios model, int position) {
+    protected void populateViewHolder(final EjerciciosHolder viewHolder, final Ejercicios model, int position) {
         viewHolder.nombre.setText(model.getNombre());
         Glide.with(contexto).load(model.getImagen()).into(viewHolder.imagen);
         viewHolder.imagen.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +40,39 @@ public class FirebaseAdapter extends FirebaseRecyclerAdapter<Ejercicios, Ejercic
                 listener.onTipoSelected(model);
             }
         });
-        viewHolder.numero.setText(String.valueOf(model.getNumero()));
+
+        // Cuenta todos los ejercicios y lo modifica en la app en tiempo real
+        switch (model.getNombre()) {
+            case "Todos":
+                FirebaseDatabase.getInstance().getReference("Ejercicios2").orderByChild("nombre").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        viewHolder.numero.setText(String.valueOf(dataSnapshot.getChildrenCount()));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                break;
+
+                default:
+                    FirebaseDatabase.getInstance().getReference("Ejercicios2").orderByChild("categoria").equalTo(model.getNombre()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            viewHolder.numero.setText(String.valueOf(dataSnapshot.getChildrenCount()));
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    break;
+        }
     }
 
     public interface OnTipoSelectedListener {
